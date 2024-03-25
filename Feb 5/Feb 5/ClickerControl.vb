c64492fd85448400f2e634c24d635a1a1eb323bd
Drew <drewmyname@gmail.com>
2024-03-25 12:23:54 -0700

    CarMods
    Group project

---
+++ Feb 5/Feb 5/ClickerControl.vb
@@ -0,0 +1,8 @@
+﻿Public Class ClickerControl
+    Dim c As New Integer
+    Public Property Increment As Integer = 1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        c.Increment = Increment
+        c.Click()
+    End Sub
+End Class
---
+++ VendingMachine/VendingMachine/CoinSlot.vb
@@ -0,0 +1,23 @@
+﻿Public Class CoinSlot
+    Public Property Quarters As Integer
+    Public Property Dimes As Integer
+    Public Property Nickels As Integer
+    Public Property Dollars As Integer
+    Public ReadOnly Property Total As Decimal
+        Get
+            Return Nickels * 0.05 + Dimes * 0.1 + Quarters * 0.25 + Dollars
+        End Get
+    End Property
+    Public Sub InsertQuarter()
+        Quarters += 1
+    End Sub
+    Public Sub InsertDime()
+        Dimes += 1
+    End Sub
+    Public Sub InsertNickel()
+        Nickels += 1
+    End Sub
+    Public Sub InsertDollar()
+        Dollars += 1
+    End Sub
+End Class
